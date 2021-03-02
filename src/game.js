9a4135c6af6f4187a82d347ad90afe17b90b3896
millina <milehi@utu.fi>
2021-03-02 20:28:01 +0200

    check area settings method done

--- src/game.js
+++ src/game.js
@@ -1,1 +1,25 @@
-console.log("game");
+const height = prompt("Peliruudukon korkeus");
+const width = prompt("Peliruudukon leveys");
+const carrier = prompt("lentotukialusten määrä");
+const battleship= prompt("taistelulaivojen määrä");
+const cruiser = prompt("risteilijöiden määrä");
+const submarine= prompt("sukellusveneiden määrä");
+const destroyer = prompt("hävittäjien määrä");
+
+/*
+Carrier	= lentotukialus
+Battleship = taistelulaiva
+Cruiser = ristelijä
+Submarine = sukellusvene
+Destroyer = hävittäjä */
+
+
+/*
+Function that checks whether the grid's area and the combined area of all of the ships are allowed
+*/
+function settingsOk (gridHeight, gridWidth, carriers, battleships, cruisers, submarines, destroyers) {
+    const gridSize = gridHeight*gridWidth;
+    const shipSize = carriers*5 + battleships*4 + cruisers*3 + submarines*3 + destroyers*2;
+    return gridSize>=shipSize*2;
+}
+
