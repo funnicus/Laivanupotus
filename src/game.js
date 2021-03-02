4d4f16aed36b968c22c409b0ba69f14899d9d148
millina <milehi@utu.fi>
2021-03-02 20:39:36 +0200

    format changes

--- src/game.js
+++ src/game.js
@@ -1,9 +1,9 @@
 const height = prompt("Peliruudukon korkeus");
 const width = prompt("Peliruudukon leveys");
 const carrier = prompt("lentotukialusten määrä");
-const battleship= prompt("taistelulaivojen määrä");
+const battleship = prompt("taistelulaivojen määrä");
 const cruiser = prompt("risteilijöiden määrä");
-const submarine= prompt("sukellusveneiden määrä");
+const submarine = prompt("sukellusveneiden määrä");
 const destroyer = prompt("hävittäjien määrä");
 
 /*
@@ -13,13 +13,11 @@ Cruiser = ristelijä
 Submarine = sukellusvene
 Destroyer = hävittäjä */
 
-
 /*
 Function that checks whether the grid's area and the combined area of all of the ships are allowed
 */
-function settingsOk (gridHeight, gridWidth, carriers, battleships, cruisers, submarines, destroyers) {
-    const gridSize = gridHeight*gridWidth;
-    const shipSize = carriers*5 + battleships*4 + cruisers*3 + submarines*3 + destroyers*2;
-    return gridSize>=shipSize*2;
+function settingsOk(gridHeight, gridWidth, carriers, battleships, cruisers, submarines, destroyers) {
+  const gridSize = gridHeight * gridWidth;
+  const shipSize = carriers * 5 + battleships * 4 + cruisers * 3 + submarines * 3 + destroyers * 2;
+  return gridSize >= shipSize * 2;
 }
-
