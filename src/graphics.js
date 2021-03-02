14f025ca2ad820c253d90e0eb64e47b249856818
Juhana Kuparinen <jmkupa@utu.fi>
2021-03-02 20:23:36 +0200

    abcdefg

--- src/graphics.js
+++ src/graphics.js
@@ -3,13 +3,26 @@ console.log("graphics");
 const p1 = document.querySelector("#p1")
 const p2 = document.querySelector("#p2")
 
-const width = 10
-const height = 10
+const width = 11
+const height = 11
+
+const chars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
 
 function createGrid(div) {
     for(let i = 0; i < width*height; i++){
         const square = document.createElement("div")
         div.appendChild(square)
+        if(i < 11 && i !== 0){
+            const p = document.createElement("p")
+            square.appendChild(p)
+            const index = i -1
+            p.innerHTML = chars[index]
+        }
+        if(i % 11 === 0 && i !== 0){
+            const p = document.createElement("p")
+            square.appendChild(p)
+            p.innerHTML = i/11
+        }
     }
 }
 
