d5ec0749b1392dd2d99debd3004ed1b7f0bc9078
Juhana Kuparinen <jmkupa@utu.fi>
2021-03-02 20:03:02 +0200

    Grids

--- src/graphics.js
+++ src/graphics.js
@@ -1,1 +1,19 @@
 console.log("graphics");
+
+const p1 = document.querySelector("#p1")
+const p2 = document.querySelector("#p2")
+
+const width = 10
+const height = 10
+
+function createGrid(div) {
+    for(let i = 0; i < width*height; i++){
+        const square = document.createElement("div")
+        div.appendChild(square)
+    }
+}
+
+createGrid(p1)
+createGrid(p2)
+
+console.log(height)
