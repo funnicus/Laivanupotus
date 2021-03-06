aeb7da0f222dcf54e184d4b8d46d4d9a7b74dfbe
mvkump <mvkump@utu.fi>
2021-03-06 19:12:41 +0200

    added hit detection function

--- src/game.js
+++ src/game.js
@@ -13,11 +13,11 @@ Destroyer = hävittäjä, pituus 2 */
 
 /**
  * Function that sets the amount of different types of ships in the game
- * @param {number} carriers 
- * @param {number} battleships 
- * @param {number} cruisers 
- * @param {number} submarines 
- * @param {number} destroyers 
+ * @param {number} carriers
+ * @param {number} battleships
+ * @param {number} cruisers
+ * @param {number} submarines
+ * @param {number} destroyers
  */
 function setShipAmounts(carriers, battleships, cruisers, submarines, destroyers) {
   ships = {
@@ -31,8 +31,8 @@ function setShipAmounts(carriers, battleships, cruisers, submarines, destroyers)
 
 /**
  * Function that checks whether the grid's area and the combined area of all of the ships are allowed
- * @param {number} gridHeight 
- * @param {number} gridWidth 
+ * @param {number} gridHeight
+ * @param {number} gridWidth
  */
 function settingsOk(gridHeight, gridWidth) {
   const gridSize = gridHeight * gridWidth;
@@ -42,16 +42,17 @@ function settingsOk(gridHeight, gridWidth) {
 
 /**
  * Function that adds a single ship to the player
- * @param {number} player 
- * @param {string} shipType 
- * @param {object} coords 
+ * @param {number} player
+ * @param {string} type
+ * @param {object} coords
  */
-function setShip(player, shipType, coords) {
+function setShip(player, type, coords) {
   const ship = {
     x: coords.x,
     y: coords.y,
     dir: coords.dir,
-    shipType,
+    type,
+    hits: 0,
   };
   if (player === 1) {
     Player1Ships.push(ship);
@@ -60,4 +61,55 @@ function setShip(player, shipType, coords) {
   } else {
     throw new Error("Invalid player number");
   }
+}
+
+/**
+ * Checks if the coordinates are inside a ship
+ * @param {object} ship
+ * @param {object} hitCoords
+ * @param {string} type
+ */
+function areCoordsOnShip(ship, hitCoords, type) {
+  let currX = ship.x;
+  let currY = ship.y;
+
+  const l = shipLength(type);
+
+  for (let i = 0; i < l; i++) {
+    switch (ship.dir) {
+      case "l":
+        currX -= 1;
+        break;
+      case "r":
+        currX += 1;
+        break;
+      case "u":
+        currY -= 1;
+        break;
+      case "d":
+        currY += 1;
+        break;
+    }
+
+    if (currX === hitCoords.x && currY === hitCoords.y) return true;
+  }
+
+  return false;
+}
+
+/**
+ * Returns the length of the given ship type
+ * @param {*} type
+ * @returns
+ */
+function shipLength(type) {
+  const sizes = {
+    carrier: 5,
+    battleship: 4,
+    cruiser: 3,
+    submarine: 3,
+    destroyer: 2,
+  };
+
+  return sizes[type];
 }
