24b8f284d9a44c38747501f60d2ac6cbfcd4f82f
Juhana Kuparinen <jmkupa@utu.fi>
2021-03-06 18:57:01 +0200

    grid size can be changed in js

--- src/graphics.js
+++ src/graphics.js
@@ -3,8 +3,12 @@ const p2 = document.querySelector("#p2")
 // ships
 const ships = document.querySelectorAll('.ship');
 
-const WIDTH = 11
-const HEIGHT = 11
+const GRID_SIDE_SIZE = 11
+const r = document.querySelector(':root');
+// Get the styles (properties and values) for the root
+const rs = getComputedStyle(r);
+// Alert the value of the --blue variable
+r.style.setProperty("--grid-size", GRID_SIDE_SIZE*40 + "px");
 
 const chars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
 
@@ -15,21 +19,21 @@ const chars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
  * @param {Element} div 
  */
 function createGrid(div) {
-    for(let i = 0; i < WIDTH*HEIGHT; i++){
+    for(let i = 0; i < GRID_SIDE_SIZE*GRID_SIDE_SIZE; i++){
         const square = document.createElement("div")
         div.appendChild(square)
         if(i === 0) continue;
-        if(i < 11 && i !== 0){
+        if(i < GRID_SIDE_SIZE){
             const p = document.createElement("p")
             square.appendChild(p)
             const index = i -1
             p.innerHTML = chars[index]
             continue;
         }
-        if(i % 11 === 0 && i !== 0){
+        if(i % GRID_SIDE_SIZE === 0 && i !== 0){
             const p = document.createElement("p")
             square.appendChild(p)
-            p.innerHTML = i/11
+            p.innerHTML = i/GRID_SIDE_SIZE
             continue;
         }
         square.addEventListener('dragenter', dragEnter);
