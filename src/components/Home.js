23cc2ded7b827a17788b773353c37ef080d0d6f2
millina <milehi@utu.fi>
2021-03-15 21:10:46 +0200

    home page :-)

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 id="title">Laivanupotus</h1>
+      <div id="buttons">
+        <div id="main-buttons">
+          <button>Aloita peli</button>
+          <button>Ohjeet</button>
+        </div>
+        <div id="credits-button">
+          <button>Tekijät</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
