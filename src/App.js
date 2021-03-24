2b890768080f195959af55923f3f9bef8b0df793
mvkump <mvkump@utu.fi>
2021-03-24 17:19:20 +0200

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import { Client } from "boardgame.io/react";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
 import Game from "./components/Game/Game";
 import GameRenderer from "./components/GameRenderer";
 
-const App = Client({ game: Game, board: GameRenderer });
+const App = () => {
+  const GameComponent = Client({ game: Game, board: GameRenderer });
+
+  return (
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact>
+            <h1>hello</h1>
+          </Route>
+          <Route path="/play">
+            <GameComponent />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
