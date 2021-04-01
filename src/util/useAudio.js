1561ced7b374d1bddf687b0ec240bb4598d003de
mvkump <mvkump@utu.fi>
2021-04-01 19:32:16 +0300

    useAudio docs

--- src/util/useAudio.js
+++ src/util/useAudio.js
@@ -1,17 +1,28 @@
 import { useEffect, useState } from "react";
 
-export const useSfx = ({ url, volume, resetOnPlay }) => {
+/**
+ * Custom React hook to play sound effects in a component.
+ * Returns two functions to start and stop playback of the audio clip.
+ * @param {object} opts { url, volume, resetOnPlay }
+ * url (string): path to audio file,
+ * volume (number): between 0 and 1
+ * @returns {[play, stop]} [play, stop]
+ */
+export const useSfx = ({ url, volume }) => {
   const [audio] = useState(new Audio(url));
   const [playing, setPlaying] = useState(false);
 
   useEffect(() => {
-    audio.volume = volume ?? 0.05;
+    audio.volume = volume ?? 0.075; // default to 7.5% volume
   }, []);
 
   useEffect(() => {
     if (resetOnPlay) audio.currentTime = 0;
     if (playing) audio.play();
-    if (!playing) audio.pause();
+    if (!playing) {
+      audio.pause();
+      audio.currentTime = 0;
+    }
   }, [playing]);
 
   // update status of playing
